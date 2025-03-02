0bde8492f025031329b96fbd9dd9e7aa138e4174
arshia <arshiaak85@gmail.com>
2025-03-02 08:03:57 -0800

    E5.18

---
+++ E5_18.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class E5_18 {
+    public static void main(String[] args) {
+        System.out.println("enter tree string");
+        Scanner input = new Scanner(System.in);
+        String[] s = new String[3];
+        for (int i = 0; i < s.length; i++) {
+            s[i] = input.nextLine();
+        }
+        Arrays.sort(s);
+
+        for (int i = 0; i < s.length; i++) {
+            System.out.println(s[i]);
+        }
+    }
+}
