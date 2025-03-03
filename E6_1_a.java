0ee12a190bf16e08c643fbc942ad1b572a8dfa94
arshia <arshiaak85@gmail.com>
2025-03-02 22:44:52 -0800

    E6.1(a)

---
+++ E6_1_a.java
@@ -0,0 +1,10 @@
+public class E6_1_a {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 2; i <= 100; i += 2) {
+            sum += i;
+        }
+
+        System.out.println("Sum of even numbers between 2 and 100 is : " + sum);
+    }
+}
