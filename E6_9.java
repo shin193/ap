18a261d65f8a5f7f328cc589ac925ffba86fbc7a
arshia <arshiaak85@gmail.com>
2025-03-07 01:09:13 -0800

    ex

---
+++ E6_9.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class E6_9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter a sentence : ");
+        String sen = sc.nextLine();
+        for (int i = sen.length()-1 ; i >= 0; i--) {
+            System.out.print(sen.charAt(i));
+
+        }
+    }
+}
