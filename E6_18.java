76877bbf26db7a697214ebd9462fbc7eb5b198b3
arshia <arshiaak85@gmail.com>
2025-03-07 01:54:03 -0800

    ex

---
+++ E6_18.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class E6_18 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter the length: ");
+        int n = sc.nextInt();
+
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
