25c0ce446edd6ca2a50fabc86ff32c19f97d02f5
arshia <arshiaak85@gmail.com>
2025-03-07 01:31:34 -0800

    ex

---
+++ E6_16.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class E6_16 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <11; i++) {
+            for (int j = 1; j <11 ; j++) {
+                System.out.printf("%6d",i*j);
+            }
+            System.out.print("\n");
+        }
+    }
+}
