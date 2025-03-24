7bd5ded9e583a8204eee83803d0d01dc564890a6
arshia <arshiaak85@gmail.com>
2025-03-24 14:52:10 -0700

    exercise2

--- Exercise2/EX2_PM_1_1.java
+++ Exercise2/EX2_PM_1_1.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class EX2_PM_1_1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number : ");
+        System.out.print("Enter a number: ");
         int k = sc.nextInt();
         int size = k + 2;
         for (int i = 0; i < size; i++) {
