237f2334b5dcfd103a0767b4dcf60e6a9e78093d
arshia <arshiaak85@gmail.com>
2025-03-26 13:20:04 -0700

    exercise2

---
+++ Exercise2/EX2_PM_1_4.java
@@ -0,0 +1,37 @@
+package Exercise2;
+
+import java.util.Scanner;
+
+public class EX2_PM_1_4 {
+    public static void main(String[] args) {
+        System.out.println("Enter an action: ");
+        Scanner sc = new Scanner(System.in);
+        while (true){
+            String action = sc.nextLine();
+        switch (action) {
+            case "w":{
+                System.out.println("up");
+                break;}
+            case "s":{
+                System.out.println("down");
+                break;}
+            case "a":{
+                System.out.println("left");
+                break;
+            }
+            case "d":{
+                System.out.println("right");
+                break;
+            }
+            case "q":{
+                System.out.println("quit");
+                return;
+            }
+            default:{
+                System.out.println("Invalid action");
+                break;
+            }
+        }
+    }
+    }
+}
