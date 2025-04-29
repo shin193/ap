51da3568b0612ea4964eacbbb4e3f4e243d35dd2
arshia <arshiaak85@gmail.com>
2025-04-29 11:39:01 -0700

    this commit is for Ex3_12.

---
+++ Exercise/Ex4/E3_12/Employee.java
@@ -0,0 +1,23 @@
+package Exercise.Ex4.E3_12;
+
+public class Employee {
+    private String name;
+    private double salary;
+
+    public Employee(String employeeName, double currentSalary) {
+        name = employeeName;
+        salary = currentSalary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void raiseSalary(double byPercent) {
+        salary += salary * byPercent / 100;
+    }
+}
